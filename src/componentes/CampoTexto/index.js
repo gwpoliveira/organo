21831818d0f1d1fc3de063390be085582d900a9e
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-06-27 15:49:10 -0300

    Subindo

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+import './CampoTexto.css'
+const CampoTexto = (props) => {
+    return(
+        <div className="campoTexto">
+            <label>{props.label}</label>
+            <input placeholder={`${props.placeholder}...`}/>
+        </div>
+    )
+}
+export default CampoTexto;
